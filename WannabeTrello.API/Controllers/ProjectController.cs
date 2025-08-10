d35e5660ffa8777e28da6c3f1e4a10d3fad5ab6f
Marko Samoilovic <msamoilovic@gmail.com>
2025-08-10 12:10:30 +0200

    Added Action for Updating Project

--- WannabeTrello.API/Controllers/ProjectController.cs
+++ WannabeTrello.API/Controllers/ProjectController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WannabeTrello.Application.Features.Projects.CreateProject;
+using WannabeTrello.Application.Features.Projects.UpdateProject;
 
 namespace WannabeTrello.Controllers;
 
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
-public class ProjectController(IMediator mediator): ControllerBase
+public class ProjectController(IMediator mediator) : ControllerBase
 {
     /// <summary>
     /// Creates a new project within the application.
@@ -26,5 +27,20 @@ public class ProjectController(IMediator mediator): ControllerBase
     {
         var projectId = await mediator.Send(command);
         return CreatedAtAction(nameof(CreateProject), new { id = projectId }, projectId);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateProject(int id, [FromBody] UpdateProjectCommand command)
+    {
+        if (id != command.ProjectId)
+        {
+            return BadRequest("ID u URL-u mora se podudarati sa ID-om u telu zahteva.");
+        }
+        
+        return Ok(await mediator.Send(command));
     }
 }
--- WannabeTrello.Application/Features/Projects/UpdateProject/UpdateProjectCommand.cs
+++ WannabeTrello.Application/Features/Projects/UpdateProject/UpdateProjectCommand.cs
@@ -4,6 +4,7 @@ using WannabeTrello.Domain.Enums;
 namespace WannabeTrello.Application.Features.Projects.UpdateProject;
 
 public record UpdateProjectCommand(
+    long ProjectId,
     string? Name,
     string? Description,
     ProjectStatus Status,
