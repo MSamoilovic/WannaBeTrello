65869038f8f5a60ee02520ec8cf09e48b31f4036
Marko Samoilovic <msamoilovic@gmail.com>
2025-08-14 14:05:02 +0200

    Added missing events for project entity

---
+++ WannabeTrello.Application/Features/Events/Project/ProjectArchivedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using WannabeTrello.Application.Common.Interfaces;
+using WannabeTrello.Domain.Events.Project_Events;
+
+namespace WannabeTrello.Application.Features.Events.Project;
+
+public class ProjectArchivedEventHandler(
+    IProjectNotificationService projectNotificationService
+): INotificationHandler<ProjectArchivedEvent>
+{
+    public async Task Handle(ProjectArchivedEvent notification, CancellationToken cancellationToken)
+    {
+        await projectNotificationService.NotifyProjectArchived(notification.ProjectId, notification.ModifierId);
+    }
+}
---
+++ WannabeTrello.Application/Features/Events/Project/ProjectMemberAddedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using WannabeTrello.Application.Common.Interfaces;
+using WannabeTrello.Domain.Events.Project_Events;
+
+namespace WannabeTrello.Application.Features.Events.Project;
+
+public class ProjectMemberAddedEventHandler(
+    IProjectNotificationService projectNotificationService
+): INotificationHandler<ProjectMemberAddedEvent>
+{
+    public async Task Handle(ProjectMemberAddedEvent notification, CancellationToken cancellationToken)
+    {
+        await projectNotificationService.NotifyProjectMemberAdded(
+            notification.ProjectId,
+            notification.NewMemberId,
+            "",
+            notification.InviterUserId
+        );
+    }
+}
