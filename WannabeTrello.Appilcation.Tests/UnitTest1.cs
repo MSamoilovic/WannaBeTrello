e349e7a9e83018bb18fcf2bd0a3b2639e6559350
Marko Samoilovic <msamoilovic@gmail.com>
2025-08-16 09:59:20 +0200

    Added Testing Projects

---
+++ WannabeTrello.Appilcation.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WannabeTrello.Appilcation.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ WannabeTrello.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WannabeTrello.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
