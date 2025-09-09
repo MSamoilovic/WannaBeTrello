60e2777bba2fba44daa82bf1828923d3519e7105
Marko Samoilovic <msamoilovic@gmail.com>
2025-09-09 14:28:15 +0200

    Added migration

---
+++ WannabeTrello.Infrastructure/Migrations/20250908125118_Added_Jsonb_.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WannabeTrello.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Jsonb_ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+    ALTER TABLE ""ActivityTrackers""
+    ALTER COLUMN ""OldValue"" TYPE jsonb
+    USING ""OldValue""::jsonb;
+
+    ALTER TABLE ""ActivityTrackers""
+    ALTER COLUMN ""NewValue"" TYPE jsonb
+    USING ""NewValue""::jsonb;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+    ALTER TABLE ""ActivityTrackers""
+    ALTER COLUMN ""OldValue"" TYPE text;
+
+    ALTER TABLE ""ActivityTrackers""
+    ALTER COLUMN ""NewValue"" TYPE text;
+");
+        }
+    }
+}
