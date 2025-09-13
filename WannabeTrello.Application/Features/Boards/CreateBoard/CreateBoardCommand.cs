e82557889cf0d4b1ac1d7cce59876dab4ac2f0bc
Marko Samoilovic <msamoilovic@gmail.com>
2025-09-13 20:01:19 +0200

    Started tests for Board applications

--- WannabeTrello.Application/Features/Boards/CreateBoard/CreateBoardCommand.cs
+++ WannabeTrello.Application/Features/Boards/CreateBoard/CreateBoardCommand.cs
@@ -2,7 +2,7 @@
 
 namespace WannabeTrello.Application.Features.Boards.CreateBoard;
 
-public class CreateBoardCommand : IRequest<long>
+public class CreateBoardCommand : IRequest<CreateBoardCommandResponse>
 {
     public long ProjectId { get; set; }
     public string Name { get; set; } = null!;
