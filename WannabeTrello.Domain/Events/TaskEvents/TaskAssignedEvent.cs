cfcf3ada038d16f821f3e418c486b00209c9e9c3
Marko Samoilovic <msamoilovic@gmail.com>
2025-10-09 19:11:07 +0200

    Started work on BoardTask entity

---
+++ WannabeTrello.Domain/Events/TaskEvents/TaskAssignedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace WannabeTrello.Domain.Events.TaskEvents;
+
+public class TaskAssignedEvent(long taskId, long? oldAssigneeId, long? newAssigneeId, long assignedByUserId) : DomainEvent
+{
+    public long TaskId => taskId;
+    public long? OldAssigneeId => oldAssigneeId;
+    public long? NewAssigneeId => newAssigneeId;
+    public long AssignedByUserId => assignedByUserId;
+}
