13a62636c435b03f8913ae7815f71774bb2489f7
Marko Samoilović <msamoilovic@gmail.com>
2025-10-27 20:08:04 +0100

    Feature/task management (#54)
    * feat(columns): Added Delete Column functionality
    
    * Added Column Delete Validator

---
+++ WannabeTrello.Infrastructure/Migrations/20251027184524_AddedIsDeletedToColumns.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WannabeTrello.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsDeletedToColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Columns",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Columns");
+        }
+    }
+}
