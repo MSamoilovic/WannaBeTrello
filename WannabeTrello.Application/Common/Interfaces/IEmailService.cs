01e9cab1710c29402d37efce92a6ef0be0675a73
Marko Samoilovic <msamoilovic@gmail.com>
2025-11-17 16:50:28 +0100

    feat(forgotpassword): Added EmailService

---
+++ WannabeTrello.Application/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿namespace WannabeTrello.Application.Common.Interfaces;
+
+public interface IEmailService
+{
+    Task SendPasswordResetEmailAsync(
+    string toEmail,
+    string userName,
+    string resetUrl,
+    CancellationToken cancellationToken = default);
+
+    Task SendPasswordResetConfirmationEmailAsync(
+        string toEmail,
+        string userName,
+        CancellationToken cancellationToken = default);
+}
